21aefd3af919738123fcb4a39c226176b863f664
Shubhay Yadav <shubhayyadav84@example.com>
2026-05-31 18:53:41 +0530

    refactor: move client source code inside src directory and images inside public directory to align with index.html paths

--- server/routes/userroute.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-
-import isAuth from "../middleware/isauth.js"
-import { getCurrentUser } from "../controllers/user.controller.js"
-
-const userRouter = express.Router()
-
-userRouter.get("/me", isAuth, getCurrentUser)
-export default userRouter
